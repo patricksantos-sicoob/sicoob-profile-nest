6f0fb80b56edd19cea13744f400489f33770fd38
Patrick Girardi dos Santos <patrick.santos@segurosicoob.com.br>
2024-10-14 17:29:25 -0300

    feat: added enableCors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000', // URL do seu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(2345);
 }
 bootstrap();
